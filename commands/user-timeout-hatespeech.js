076a244b677e6d8e294b23a0ff08aaf8447ae583
Christopher Welch <deadclap893@gmail.com>
2023-11-01 00:11:05 -0400

    chore: full scale linting

--- commands/user-timeout-hatespeech.js
+++ commands/user-timeout-hatespeech.js
@@ -1,16 +1,16 @@
 module.exports = {
-    name: 'Mod Action: Hate Speech',
-    data: {
-        name: "Mod Action: Hate Speech",
-        type: 2
-    },
-    async execute(interaction) {
-        const target = interaction.targetMember
-        const user = await interaction.client.users.fetch(target.id)
-        const sender = await interaction.client.users.fetch(interaction.member.user.id)
-        console.log(user.id)
-        console.log(user.username)
-        await target.timeout(1800, `${sender.username} - Mod Action: Hate Speech`)
-        await interaction.reply(`Timed out: ${user.username}`)
-    },
+	name: 'Mod Action: Hate Speech',
+	data: {
+		name: 'Mod Action: Hate Speech',
+		type: 2,
+	},
+	async execute(interaction) {
+		const target = interaction.targetMember;
+		const user = await interaction.client.users.fetch(target.id);
+		const sender = await interaction.client.users.fetch(interaction.member.user.id);
+		console.log(user.id);
+		console.log(user.username);
+		await target.timeout(1800, `${sender.username} - Mod Action: Hate Speech`);
+		await interaction.reply(`Timed out: ${user.username}`);
+	},
 };
