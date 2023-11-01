1fc80c9512f612f5fbf39a80e5a2eb83124455b9
Christopher Welch <deadclap893@gmail.com>
2023-10-31 20:00:15 -0400

    patch: remove unused code

--- index.js
+++ index.js
@@ -1,54 +1,38 @@
 require('dotenv').config(); // Load environment variables from .env
 
-const { Client, Intents } = require('discord.js');
+const { Client, Intents, Collection } = require('discord.js');
 const { REST } = require('@discordjs/rest');
 const { Routes } = require('discord-api-types/v9');
 const { SlashCommandBuilder } = require('@discordjs/builders');
+const fs = require('fs')
 
 const token = process.env.DISCORD_TOKEN; // Access the bot token from the environment variable
-const clientId = process.env.CLIENT_ID;
-const guildId = process.env.GUILD_ID || null;
 
-const commands = [
-    new SlashCommandBuilder()
-        .setName('ping')
-        .setDescription('Replies with Pong!'),
-    // Add more slash commands here
-].map(command => command.toJSON());
+
 
 const client = new Client({ intents: ['Guilds'] });
 
-const rest = new REST({ version: '10' }).setToken(token);
 
 
-(async () => {
-    try {
-        console.log('Started refreshing application (/) commands.');
 
-        await rest.put(
-            Routes.applicationGuildCommands(clientId, guildId),
-            { body: commands }
-        );
 
-        console.log('Successfully reloaded application (/) commands.');
-    } catch (error) {
-        console.error(error);
+const eventFiles = fs.readdirSync('./events').filter(file => file.endsWith('.js'))
+for (const file of eventFiles) {
+    const event = require(`./events/${file}`)
+    if (event.once) {
+        client.once(event.name, (...args) => event.execute(...args, client))
+    } else {
+        client.on(event.name, (...args) => event.execute(...args, client))
     }
-})();
+}
+
+client.commands = new Collection();
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+    const command = require(`./commands/${file}`);
+    client.commands.set(command.data.name, command);
+}
 
 client.login(token);
 
-client.on('ready', () => {
-    console.log(`Logged in as ${client.user.tag}`);
-});
-
-client.on('interactionCreate', async (interaction) => {
-    if (!interaction.isCommand()) return;
-
-    const { commandName } = interaction;
-
-    if (commandName === 'ping') {
-        await interaction.reply('Pong!');
-    }
-    // Handle more interactions here
-});
